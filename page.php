e4fc5f68b01f9b15a9e69093f0267e6ca5d879aa
JulieDavoine <julie4793@hotmail.fr>
2017-10-31 14:13:36 +0100

    ajout index2.html

--- page.php
+++ page.php
@@ -1,14 +1,12 @@
-<?php
-for ($i = 0; $i <= 10; $i++)
-/*on déclare la variable de la table de multiplication, on dit qu'elle va jusqu'à dix et que c'est une boucle*/
-{
-    Echo 'Table de multiplication de '.$i.'<br>';
-    for ($j = 1; $j <= 10; $j++)/*définit la variable des nbr jusqu'à dix*/
-    
-    {
-        echo $i.' x '.$j.' = '.$i*$j.'<br>';
-        /* on affiche le nombre de la table choisit x les nombres jusqu'à dix = afficher le résultat de nbrmul x nbr*/
+<?php 
+
+function Multiplication($tableau){
+    $i=0;
+    while ($i<11){
+    echo ($tableau.'x'.$i."=".$tableau*$i."</br>");
+    $i++;
     }
-    
 }
-?>
+Multiplication(8);
+
+
